adf4c62992dcc54753d32819ccc838bc1101ebfc
Oussama Makhlouk <oussama.makhlouk@oracle.com>
2024-04-07 03:40:02 +0000

    ✨ Added icons implementation

---
+++ module/colors.go
@@ -0,0 +1,19 @@
+// Package module - module/colors.go
+package module
+
+const (
+	// Bold format
+	Bold string = "\033[1m"
+	// Reset format color code
+	Reset string = "\033[0m"
+	// LightPurple color
+	LightPurple string = "\033[1;35m"
+	// LightBlue color
+	LightBlue string = "\033[1;36m"
+	// Grey color
+	Grey string = "\033[90m"
+	// Pink color
+	Pink string = "\033[38;5;206m"
+	// Orange color
+	Orange string = "\033[38;5;208m"
+)
